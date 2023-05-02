3374bc070193b5727ccbc3bbee56ef3e087d72a5
VladimirVinageras <vvinageras@gmail.com>
2023-05-02 23:55:02 +0200

    Initial Commit

---
+++ ExclusiveChat/ExclusiveChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExclusiveChatApp.swift
+//  ExclusiveChat
+//
+//  Created by Vladimir Vinageras on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExclusiveChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
